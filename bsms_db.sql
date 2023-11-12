9b44c5a9a2aee5a62b214ae0eca4eb46f89327de
Fiza Azhar <fizaazhar54@gmail.com>
2023-11-13 01:04:31 +0500

    user Registration

---
+++ bsms_db.sql
@@ -0,0 +1,12 @@
+SET time_zone = "+00:00";
+
+CREATE TABLE `users` (
+  `id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(100) NOT NULL,
+  `user_type` varchar(20) NOT NULL DEFAULT 'user'
+) 
+
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+
+$conn = mysqli_connect('localhost','root','','bsms_db') or die('connection failed');
+
+?>
