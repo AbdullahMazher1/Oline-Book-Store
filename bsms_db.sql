1329caf3869105c7a7402cb3ff7a6f4dfac0a756
Fiza Azhar <fizaazhar54@gmail.com>
2023-11-19 22:38:23 +0500

    Orders and Users Record

--- bsms_db.sql
+++ bsms_db.sql
@@ -1,21 +1,31 @@
 SET time_zone = "+00:00";
 
 CREATE TABLE `users` (
-  `id` int(100) NOT NULL,
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(100) NOT NULL,
-  `user_type` varchar(20) NOT NULL DEFAULT 'user'
-) 
+  `user_type` varchar(20) NOT NULL DEFAULT 'user',
+  `status` varchar(50) NOT NULL DEFAULT 'Active',
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
 
-<<<<<<< HEAD
-=======
 CREATE TABLE `products` (
-  `id` int(100) NOT NULL,
+  `id` int(100) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
+  `author` varchar(100) NOT NULL,
   `price` int(100) NOT NULL,
-  `image` varchar(100) NOT NULL
-)
+  `stock` int(100) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `status` varchar(50) NOT NULL DEFAULT 'Active',
+  `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+);
+
+
 
 CREATE TABLE `cart` (
   `id` int(100) NOT NULL,
@@ -26,6 +36,18 @@ CREATE TABLE `cart` (
   `image` varchar(100) NOT NULL
 ) 
 
->>>>>>> 8b0f188 (Successful Login and database update)
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
+CREATE TABLE `orders` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `user_id` int(100) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `number` varchar(12) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `method` varchar(50) NOT NULL,
+  `address` varchar(500) NOT NULL,
+  `total_products` varchar(1000) NOT NULL,
+  `total_price` int(100) NOT NULL,
+  `placed_on` varchar(50) NOT NULL,
+  `payment_status` varchar(20) NOT NULL DEFAULT 'pending',
+  PRIMARY KEY (`id`)
+)
+
